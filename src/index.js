d671d957c633a1010216c84c46adc898b68b0537
Gabriel Halley <gabhalley@gmail.com>
2025-04-16 11:25:00 -0400

    19-191 useState vs setState

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 const App = ({ defaultCount = 0 }) => {
   const [count, setCount] = useState(defaultCount);
+  const [text, setText] = useState('');
 
   return (
     <div>
-      <p>The current count is {count}</p>
+      <p>The current {text || 'count'} is {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>-1</button>
       <button onClick={() => setCount(defaultCount)}>-Reset</button>
+      <input value={text} onChange={(e) => setText(e.target.value)}/>
     </div>
   )
 };
