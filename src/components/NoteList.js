1e302f23c34471ee96e667dc2555089499bc75b1
Gabriel Halley <gabhalley@gmail.com>
2025-04-23 16:46:11 -0400

    19-198 The Context API & useContext Hook Part 2

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Note from './notes';
+import NotesContext from '../context/notes-context';
 
-const NoteList = ({ notes, removeNote }) => {
+const NoteList = () => {
+  const { notes } = useContext(NotesContext);
+
   return notes.map((note) => (
     <Note
       key={note.title}
       note={note}
-      removeNote={removeNote}
     />
   ))
 }
